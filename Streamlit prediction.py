d2c2a2f69d7d1740262518af4d05d84a3bf70f1f
P Suresh <127506858+suresh4811@users.noreply.github.com>
2023-06-30 23:01:37 +0530

    Add files via upload

---
+++ Streamlit prediction.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import streamlit as st
+import numpy as np
+from PIL import Image
+import pickle
+import requests
+
+
+# In[8]:
+
+
+st.sidebar.subheader("Welcome to The Suri Enterprises Pvt. Ltd !")
+
+
+# In[9]:
+
+
+st.sidebar.subheader(
+        "We rent and sales cars. This company is based on second hand or branded new cars. All the facility are available here.")
+
+
+# In[10]:
+
+
+st.sidebar.write("P Suresh ", "\n", "CEO")
+
+
+# In[11]:
+
+
+st.sidebar.subheader('Contact Us.  \n'
+                         'Email:-  sureshgudboy417@gmail.com')
+
+
+# In[ ]:
+
+
+
+
